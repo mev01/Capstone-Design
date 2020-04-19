36aa4b83d2ad2a9d85ed368eb65522293d11a8b4
Mev01 <mevolicus@gmail.com>
2020-04-20 02:20:21 +0900

    tutorial

--- Music Healing VR/Assets/02.Scripts/LookItem.cs
+++ Music Healing VR/Assets/02.Scripts/LookItem.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LookItem : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class LookItem : MonoBehaviour
     private float currentTime = 0;
     bool isLookAt=false;
     bool check = true;
+
+    public Text chText;
+    private int pg = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,22 +22,34 @@ public class LookItem : MonoBehaviour
     void Update()
     {
         currentTime += Time.deltaTime;
-        if(!isLookAt)
+        if (!isLookAt)
         {
             stopTime = currentTime;
+            check = true;
         }
-        if (currentTime - stopTime >= 2&&check)
+        if (currentTime - stopTime >= 1 && check)
         {
-            Debug.Log("ch");
+            if (pg++ == 1)
+                Text1();
+            else 
+                Text2();
             check = false;
         }
-        //Debug.Log(currentTime);
-        //Debug.Log(stopTime);
     }
 
     public void onLookItemBox(bool look)
     {
         isLookAt = look;
         Debug.Log(isLookAt);
+    }
+
+    public void Text1()
+    {
+        chText.GetComponent<Text>().text = "이제부터 제가 하는 질문에 답변해주세요. 준비가 되면 next버튼을 봐주세요";
+    }
+
+    public void Text2()
+    {
+        chText.GetComponent<Text>().text = "오늘 기분이 어떠신가요??";
     }
 }
