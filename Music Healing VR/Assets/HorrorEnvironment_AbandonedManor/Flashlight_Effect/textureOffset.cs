50d4c8e335b233ad6c54acd76d14b9722f8f729e
Jeong Yj Jeong <dbwjd7wkd@naver.com>
2020-06-05 20:23:36 +0900

    3

---
+++ Music Healing VR/Assets/HorrorEnvironment_AbandonedManor/Flashlight_Effect/textureOffset.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class textureOffset : MonoBehaviour {
+
+	public float offsetSpeed = 0.1f;
+	public string propertyName = "";
+	private Material myMat;
+
+	void Start()
+	{
+		myMat = gameObject.GetComponent<Renderer> ().material;
+	}
+
+	void Update()
+	{
+		myMat.SetTextureOffset (propertyName,new Vector2( myMat.GetTextureOffset(propertyName).x + Time.deltaTime*offsetSpeed, 0f) );
+	}
+}
